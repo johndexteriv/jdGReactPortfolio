76e64c06a7cb98f896a4fcc356e4e62689b64f33
John Garner <jdextergarneriv@gmail.com>
2021-02-04 16:44:02 -0500

    created asgm portfolio details page, created data tree for asgm, created a template to display asgm data

--- src/pages/portfolio/details/asgm.js
+++ src/pages/portfolio/details/asgm.js
@@ -2,14 +2,14 @@ import React, { Fragment } from "react";
 import Header from "../../../components/header/HeaderOne";
 import SideHeader from "../../../components/SideHeader";
 import FooterThree from "../../../components/footer/FooterThree";
-import TemplatePortfolioDetails from "../../../templates/portfolio/details";
+import ASGMPortfolioDetails from "../../../templates/portfolio/details/asgmDetails";
 
 const ASGMDetailsPage = () => {
 	return (
 		<Fragment>
 			<Header classes={"position-static"} />
 			<SideHeader mobile={true} />
-			<TemplatePortfolioDetails />
+			<ASGMPortfolioDetails />
 			<FooterThree position="fixed" />
 		</Fragment>
 	);
