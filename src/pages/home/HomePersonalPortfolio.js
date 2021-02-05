c240d40136c3067175ccc9821e7166fd664de6c2
John Garner <jdextergarneriv@gmail.com>
2021-02-05 17:57:57 -0500

    remove unwanted imports

--- src/pages/home/HomePersonalPortfolio.js
+++ src/pages/home/HomePersonalPortfolio.js
@@ -2,25 +2,17 @@ import React from "react";
 
 // File imported
 import Header from "../../components/header/HeaderOne";
-// import SideHeader from "../../components/SideHeader";
 import BannerPersonalPortfolio from "../../components/HeroBanner/PersonalPortfolio";
 import PersonalPortfolioService from "../../container/service";
 import Footer from "../../components/footer/FooterThree";
-// import PersonalPortfolio from "../../container/portfolio/PersonalPortfolio";
-// import PersonalPortfolioTeam from "../../container/team";
-// import Contact from "../../container/contact";
 
 const HomePersonalPortfolio = () => {
 	return (
 		<div className={"main-wrapper p-0"}>
 			<Header classes={"position-static"} />
-			{/* <SideHeader mobile={true} /> */}
 			<BannerPersonalPortfolio />
 			<PersonalPortfolioService />
 			<Footer position={"static"} />
-			{/* <PersonalPortfolio /> */}
-			{/* <PersonalPortfolioTeam />
-			<Contact /> */}
 		</div>
 	);
 };
